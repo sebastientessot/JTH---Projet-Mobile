acbe12ce87df0ef67772216b7619557953982cfa
Carla <carla.brtl@gmail.com>
2012-11-29 16:51:55 +0100

    Recipe pages creation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 var menuIsVisible = false;
+var recipesList = document.getElementsByClassName("recipe");
+var checkList  = document.getElementsByTagName("li");
 
 function goToMenu(event){
 	if(menuIsVisible == false){
@@ -19,6 +21,44 @@ function backToContent(event){
 	event.preventDefault();
 }
 
+function goToPage(elmt){
+	while(elmt.getAttribute('class') != "recipe"){
+		elmt = elmt.parentNode;
+	}
+	if(elmt.getAttribute("id") == 1 ){
+		window.location = "recipe1.html";
+	}
+	if(elmt.getAttribute("id") == 2 ){
+		window.location = "recipe2.html";
+	}
+	if(elmt.getAttribute("id") == 3 ){
+		window.location = "recipe3.html";
+	}
+	if(elmt.getAttribute("id") == 4 ){
+		window.location = "recipe4.html";
+	}
+	if(elmt.getAttribute("id") == 5 ){
+		window.location = "recipe5.html";
+	}
+}
 
+function checkTheBox(elmt){
+	elmt.setAttribute("style", "background-image: url(\"picture/Check.png\"); background-size: cover;");
+	elmt.innerHTML = "";
+}
+
+
+for(var i =0 ; i < recipesList.length; i++){
+	recipesList[i].addEventListener("touch", function (event){goToPage(event.target);event.preventDefault();}, false); 	
+	recipesList[i].addEventListener("click", function (event){goToPage(event.target);}, false); 	
+}
+
+for(var i =0 ; i < checkList.length; i++){
+	checkList[i].addEventListener("touch", function (event){checkTheBox(event.target);event.preventDefault();}, false); 	
+	checkList[i].addEventListener("click", function (event){checkTheBox(event.target);}, false); 	
+}
+
+document.getElementById("menuButton").addEventListener("click", function (event) {goToMenu(event)}, false); 
+document.getElementById("menuButton").addEventListener("click", function(event){goToMenu(event); event.preventDefault();}, false); 
 
 document.body.addEventListener("mousedown", backToContent ,false);
