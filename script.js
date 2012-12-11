e5fca5a67d25daf30abd12dfbcd6bdf9de15367c
Sébastien Tessot <sebastien.tessot@gmail.com>
2012-12-11 16:36:28 +0100

    add Menu link
    +add "Home" to menu

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function goToMenu(event){
 		document.body.style.overflowY = "hidden";
 		menuIsVisible = true;
 		
-		
 		for(var i =0 ; i < recipesList.length; i++){
 			recipesList[i].removeEventListener("click", goToPage, false);
 			recipesList[i].removeEventListener("touchstart", goToPage, false);
@@ -29,6 +28,38 @@ function backToContent(event){
 	event.preventDefault();
 }
 
+function goToCocktail(event){
+	window.location = "cocktail.html";
+	event.preventDefault();
+}
+
+function goToAppetizer(event){
+	window.location = "appetizer.html";
+	event.preventDefault();
+}
+
+function goToEntree(event){
+	window.location = "entree.html";
+	event.preventDefault();
+}
+
+function goToDessert(event){
+	window.location = "dessert.html";
+	event.preventDefault();
+}
+
 
 document.getElementById("content").addEventListener("mousedown", backToContent ,false);
 document.getElementById("content").addEventListener("touchstart", backToContent ,false);
+
+document.getElementById("cocktail").addEventListener("mousedown", goToCocktail ,false);
+document.getElementById("cocktail").addEventListener("touchstart", goToCocktail ,false);
+
+document.getElementById("appetizer").addEventListener("mousedown", goToAppetizer ,false);
+document.getElementById("appetizer").addEventListener("touchstart", goToAppetizer ,false);
+
+document.getElementById("entree").addEventListener("mousedown", goToEntree ,false);
+document.getElementById("entree").addEventListener("touchstart", goToEntree ,false);
+
+document.getElementById("dessert").addEventListener("mousedown", goToDessert ,false);
+document.getElementById("dessert").addEventListener("touchstart", goToDessert ,false);
